bb01519032799ec33f4000058abaed4a770b289a
Wenchen Lie <438171766@qq.com>
2025-02-10 16:02:51 +0800

    优化层高输出

--- MRFHelper/__init__.py
+++ MRFHelper/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '2.3'
+__version__ = '2.4'
 print()
 print('Tcl script generator for analysis of moment resisting frame using OpenSees')
 print('Developed by: Wenchen Lie, Guangzhou University, China')
